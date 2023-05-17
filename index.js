34b50d8b8f83516beec41cd29cf25baeaea5f83e
Vukipoo <perovicv515@gmail.com>
2023-05-16 23:05:31 -0700

    fibonacci non recursive

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+    function fibs(n) {
+        const arr = [0, 1]
+        for(let i = 2; i < n; i++){
+            arr[i] = arr[i - 1] + arr[i - 2]
+        }
+        return arr
+
+    }
+
+    console.log(fibs(8))
